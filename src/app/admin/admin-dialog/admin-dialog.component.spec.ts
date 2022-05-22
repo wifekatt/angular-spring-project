adba7f0efd8854db74478e358457d9c5dec1b412
wifekatt <wifek.attia.2000@gmail.com>
2022-05-22 15:13:58 +0100

    initial commit

---
+++ src/app/admin/admin-dialog/admin-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminDialogComponent } from './admin-dialog.component';
+
+describe('AdminDialogComponent', () => {
+  let component: AdminDialogComponent;
+  let fixture: ComponentFixture<AdminDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminDialogComponent } from './admin/admin-dialog/admin-dialog.component';
+import { AdminComponent } from './admin/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDialogComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
